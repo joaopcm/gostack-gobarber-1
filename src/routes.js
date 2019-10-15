cbdf29d66bb27072a576c46885f40a740a72f162
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-14 23:28:24 -0300

    database instance

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'ok' });
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Raphael',
+    email: 'raphael@gmail.com',
+    password_hash: 'sad564as65d4a65s4d65as4d',
+  });
+  response.json(user);
 });
 
 export default routes;
