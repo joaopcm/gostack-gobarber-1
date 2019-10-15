79fbd960ba105e8df66c460c36dbe3e63f2371b4
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-14 23:45:21 -0300

    bcrypt and model hooks

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,9 +5,12 @@ class UserController {
     const userExists = await User.findOne({
       where: { email: request.body.email },
     });
+
     if (userExists)
       return response.status(400).json({ error: 'User already exists' });
+
     const { id, name, email, provider } = await User.create(request.body);
+
     return response.json({ id, name, email, provider });
   }
 }
